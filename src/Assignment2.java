8ed84bf00505d4b2fcfd60b713f1c8981f435409
Rik Bose <88793134+infiniterik@users.noreply.github.com>
2023-04-02 00:17:24 +0000

    Sun, Apr 2, 2023, 12:17 AM +00:00

---
+++ src/Assignment2.java
@@ -0,0 +1,7 @@
+public class Assignment2 {
+   public static boolean isAnagram(String a, String b){
+        return false;
+   }
+
+   
+}
--- src/Foo.java
+++
@@ -1,9 +0,0 @@
-public class Foo {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
-
-    public static boolean foo(){
-        return false;
-    }
-}
