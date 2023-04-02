905ec832e920032ce7cfb6df5adf49150bcfff4a
Rik Bose <88793134+infiniterik@users.noreply.github.com>
2023-04-02 00:28:41 +0000

    Sun, Apr 2, 2023, 12:28 AM +00:00

--- src/Assignment2.java
+++ src/Assignment2.java
@@ -1,7 +1,50 @@
 public class Assignment2 {
-   public static boolean isAnagram(String a, String b){
+    /**
+     * Question 1
+     */
+    public static boolean isAnagram(String a, String b){
         return false;
-   }
+    }
 
-   
+    /**
+     * Question 2
+     */
+    public static int[] addsUp(int x, int[] array) {
+        return new int[2];
+    }
+
+    /**
+     * Question 3
+     */
+    public static int oddTimes(int[] array) {
+        return -1;
+    }
+
+    /**
+     * Question 4
+     */
+    public static boolean sumOfZero(int[] array) {
+        return false;
+    }
+
+    /**
+     * Question 5
+     */
+    public static int[][] areReverses(int[] array) {
+        return new int[0][0];
+    }
+
+    /**
+     * Question 6
+     */
+    public static int[] inOrderOfAppearance(int[] array){
+        return array;
+    }
+
+    /**
+     * Question 7
+     */
+    public static boolean secretCode(String a, String b) {
+        return false;
+    }
 }
