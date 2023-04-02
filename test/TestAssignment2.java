909c35348b4d11c3dec5eb905baa660f33e732fe
Rik Bose <88793134+infiniterik@users.noreply.github.com>
2023-04-02 00:48:32 +0000

    Sun, Apr 2, 2023, 12:48 AM +00:00

--- test/TestAssignment2.java
+++ test/TestAssignment2.java
@@ -3,18 +3,19 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 import java.util.List;
 
 public class TestAssignment2 {
-    static String baseDir = "/workspaces/assignment2/csv/"
+    static String baseDir = "/workspaces/assignment2/csv/";
 
-    public void prepareFiles() {
-        String anagramTests = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRmL8b78ABiAtM5sgbHrgMWvLl1LvhX-kwN54mjB2TC-goKYedPlHDm-oF9NQi8IvHM0_Iix7sn7bwy/pub?gid=0&single=true&output=csv"
-        FileDownloader.downloadFile(anagramTests, baseDir+"anagramTests.csv")
+    @BeforeClass
+    public static void prepareFiles() {
+        String anagramTests = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRmL8b78ABiAtM5sgbHrgMWvLl1LvhX-kwN54mjB2TC-goKYedPlHDm-oF9NQi8IvHM0_Iix7sn7bwy/pub?gid=0&single=true&output=csv";
+        FileDownloader.downloadFile(anagramTests, baseDir+"anagramTests.csv");
 
     }
 
     @ParameterizedTest
     @CsvFileSource(files = baseDir + "anagramTests.csv", numLinesToSkip = 1)
-    public void testAnagrams(String word1, String word2, String anagram) {
-        assert true;
+    public void testAnagrams(String word1, String word2, boolean anagram) {
+        assert Assignment2.isAnagram(word1, word2) == anagram;
     }
 
 }
