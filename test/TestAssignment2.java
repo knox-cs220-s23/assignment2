f5ebe26e3f8112a59aa39b207156f2c8dea9c81f
Rik Bose <88793134+infiniterik@users.noreply.github.com>
2023-04-02 00:50:42 +0000

    Sun, Apr 2, 2023, 12:50 AM +00:00

--- test/TestAssignment2.java
+++ test/TestAssignment2.java
@@ -1,11 +1,14 @@
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.api.BeforeAll;
+
 import java.util.List;
 
 public class TestAssignment2 {
-    static String baseDir = "/workspaces/assignment2/csv/";
+    static final String baseDir = "/workspaces/assignment2/csv/";
+    static final String anagramTests = baseDir + "anagramTests.csv";
 
-    @BeforeClass
+    @BeforeAll
     public static void prepareFiles() {
         String anagramTests = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRmL8b78ABiAtM5sgbHrgMWvLl1LvhX-kwN54mjB2TC-goKYedPlHDm-oF9NQi8IvHM0_Iix7sn7bwy/pub?gid=0&single=true&output=csv";
         FileDownloader.downloadFile(anagramTests, baseDir+"anagramTests.csv");
@@ -13,7 +16,7 @@ public class TestAssignment2 {
     }
 
     @ParameterizedTest
-    @CsvFileSource(files = baseDir + "anagramTests.csv", numLinesToSkip = 1)
+    @CsvFileSource(files = anagramTests, numLinesToSkip = 1)
     public void testAnagrams(String word1, String word2, boolean anagram) {
         assert Assignment2.isAnagram(word1, word2) == anagram;
     }
